e50c5dd6d9e4ca47f123f4ee9a95d7d516639e6f
shohagroy <pkshohag240@gmail.com>
2022-10-05 01:58:27 +0600

    project complited

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState } from "react";
 
+import logo from '../../assets/logo.png';
+
+import {Link} from 'react-scroll';
+
 const Header = () => {
 
     const [toggle, setToggle] = useState(false);
@@ -8,7 +12,7 @@ const Header = () => {
   return (
     <div className="bg-[#0a192f] text-white fixed w-full ">
       <nav className="flex justify-between h-20 items-center px-10 m-auto">
-        <img src="" alt="Logo-HTML" className="z-10" />
+        <img src={logo} alt="Logo-HTML" className="z-10 w-28" />
 
         <span onClick={()=> setToggle(!toggle)} className="md:hidden block cursor-pointer z-10">
         {
@@ -33,47 +37,49 @@ const Header = () => {
         }
       </span>
       <ul className={`${toggle ? 'top-20' : 'top-[-800px]'} z-1 duration-500 bg-[#0a192f] md:flex absolute md:static left-0 text-center top-20 w-full md:w-auto`}>
-          <li className="md:py-2 md:px-5 md:text-sm text-2xl py-10 m-5 md:m-0">
-            <a href="">Home</a>
+          <li className="md:py-2 md:px-5 md:text-sm text-2xl hover:text-[orangered] py-10 m-5 md:m-0 cursor-pointer">
+            <Link to='main' smooth={true} duration={500} >Home</Link>
           </li>
-          <li className="md:py-2 md:px-5 md:text-sm text-2xl py-10 m-5 md:m-0">
-            <a href="">About</a>
+          <li className="md:py-2 md:px-5 md:text-sm text-2xl hover:text-[orangered] py-10 m-5 md:m-0 cursor-pointer">
+          <Link to='about' smooth={true} duration={500} >About</Link>
           </li>
-          <li className="md:py-2 md:px-5 md:text-sm text-2xl py-10 m-5 md:m-0">
-            <a href="">Experience</a>
+          <li className="md:py-2 md:px-5 md:text-sm text-2xl hover:text-[orangered] py-10 m-5 md:m-0 cursor-pointer">
+          <Link to='skills' smooth={true} duration={500} >Skills</Link>
           </li>
-          <li className="md:py-2 md:px-5 md:text-sm text-2xl py-10 m-5 md:m-0">
-            <a href="">Work</a>
+          <li className="md:py-2 md:px-5 md:text-sm text-2xl hover:text-[orangered] py-10 m-5 md:m-0 cursor-pointer">
+          <Link to='work' smooth={true} duration={500} >Work</Link>
           </li>
-          <li className="md:py-2 md:px-5 md:text-sm text-2xl py-10 m-5 md:m-0">
-            <a href="">Contact</a>
+          <li className="md:py-2 md:px-5 md:text-sm text-2xl hover:text-[orangered] py-10 m-5 md:m-0 cursor-pointer">
+          <Link to='contact' smooth={true} duration={500} >Contact</Link>
           </li>
         </ul>
       </nav>
-      <div className="hidden md:block fixed top-[50%] flex flex-col w-[160px]">
-        
-        <div className="p-2 bg-blue-500 text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 ">
-            <a className="flex justify-between px-2 items-center" href="">Linkedin
+
+
+      <div className="hidden md:block fixed top-[30%] flex flex-col w-[160px]">
+
+        <div className="p-2 bg-blue-500 text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 hover:border-[orangered] ">
+            <a className="flex justify-between px-2 items-center" href="https://www.linkedin.com/in/shohag-roy-850a82236/" target='_'>Linkedin
             <i className="fa-brands fa-linkedin text-3xl pl-5 p-2"></i></a>
         </div>
 
-        <div className="p-2 bg-[#333333] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 ">
-            <a className="flex justify-between px-2 items-center" href="">GitHub
+        <div className="p-2 bg-[#333333] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 hover:border-[orangered] ">
+            <a className="flex justify-between px-2 items-center" href="https://github.com/shohagroy" target='_'>GitHub
             <i className="fa-brands fa-github text-3xl pl-5 p-2"></i></a>
         </div>
 
-        <div className="p-2 bg-[#6fc2b0] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 ">
-            <a className="flex justify-between px-2 items-center" href="">Email
+        <div className="p-2 bg-[#6fc2b0] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 hover:border-[orangered] ">
+            <a className="flex justify-between px-2 items-center" href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzCMcnzvfQCcsZmpPlqRdzTVPdHmhjWSnCvLRndRfcjDLSkSfTngDxnMjQFkCwkchdLXHJM" target='_'>Email
             <i className="fa-regular fa-envelope text-3xl pl-5 p-2"></i></a>
         </div>
 
-        <div className="p-2 bg-blue-700 text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 ">
-            <a className="flex justify-between px-2 items-center" href="">Facebook
+        <div className="p-2 bg-blue-700 text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 hover:border-[orangered] ">
+            <a className="flex justify-between px-2 items-center" href="https://www.facebook.com/shohagroy.7771/" target='_'>Facebook
             <i className="fa-brands fa-square-facebook text-3xl pl-5 p-2"></i></a>
         </div>
 
-        <div className="p-2 bg-[#565f69] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4">
-            <a className="flex justify-between px-2 items-center" href="">Facebook
+        <div className="p-2 bg-[#565f69] text-white relative left-[-90px] hover:left-0 h-16 duration-300 border-r-4 hover:border-[orangered]">
+            <a className="flex justify-between px-2 items-center" href="https://doc-0s-7c-apps-viewer.googleusercontent.com/viewer/secure/pdf/1ujdt653q9d4tf2qg8n4s0j1ol5g90gv/vv84m10ak5db5427bn31s5vttvpmbe35/1664911425000/drive/06628696055127066530/ACFrOgB9Y8gBRb5yhGe-Rdjdrp_pzfQwTeZr-HwX3XUxcw1bVuYlXPn62r8X4vCDPgFvMua3xwjLfzUnAYgBBzC_N8_CBbVJZEIV3PgMs1qriwwA2LEf1fQy4NLPYn0dFw_jCmt-yEGlSu7sIDGV?print=true&nonce=lkbrbd0b3j944&user=06628696055127066530&hash=nam0ri0nhfc4pv9ql4et6iu87lu7gsbe" target='_'>CV
             <i className="fa-solid fa-file-pdf text-3xl pl-5 p-2"></i></a>
         </div>
 
